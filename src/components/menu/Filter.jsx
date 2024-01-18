ff81b08c40ce9f4c067085ca70d2671493be3ef0
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-01-18 21:47:31 +0200

    add filter functionalities

--- src/components/menu/Filter.jsx
+++ src/components/menu/Filter.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CountriesContext } from "../../pages/Main";
 
 const Filter = () => {
   const [showFilter, setShowFilter] = useState(false);
+  const { setCountries } = useContext(CountriesContext);
+
   return (
     <div onClick={() => setShowFilter(!showFilter)}>
       <div className="flex gap-3 relative select-none">
@@ -24,35 +27,46 @@ const Filter = () => {
           <div className="select-none absolute top-16 text-sm bg-white text-gray-950 rounded-lg shadow-md p-4">
             <h3 className="text-nowrap">By continent</h3>
             <hr className="my-2" />
-            <div className="cursor-pointer space-y-2 text-nowrap">
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                Africa
-              </p>
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                North America
-              </p>
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                South America
-              </p>
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                Asia
-              </p>
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                Europe
-              </p>
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                Oceania
-              </p>
-              <p className="flex items-center gap-2">
-                <input type="checkbox" name="" id="" />
-                Antarctica
-              </p>
+            <div className="space-y-2 text-nowrap">
+              {[
+                "Africa",
+                "North America",
+                "South America",
+                " Asia",
+                " Europe",
+                " Oceania",
+                "Antarctica"
+              ].map((countryName, index) => (
+                <p
+                  className="flex items-center gap-2 cursor-pointer "
+                  key={index}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setCountries((current) => {
+                      return {
+                        ...current,
+                        filterBy: [
+                          ...current.filterBy,
+                          current.filterBy.includes(countryName)
+                            ? null
+                            : countryName
+                        ]
+                      };
+                    });
+                  }}
+                >
+                  <input
+                    type="checkbox"
+                    name={countryName}
+                    id={index}
+                    className="cursor-pointer "
+                    onClick={(e) => {
+                      e.target.checked ? console.log(true) : console.log(false);
+                    }}
+                  />
+                  {countryName}
+                </p>
+              ))}
             </div>
           </div>
         )}
