1c7a4ef0e75c24e50e6d31672301c0a00cc8f56b
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-01-20 19:53:07 +0200

    started food template

---
+++ bread.js
@@ -0,0 +1,20 @@
+// var Usrdata = document.querySelector('.box');
+
+var galleryData = [
+    {src:"https://www.w3schools.com/w3images/sandwich.jpg",heading:"The Perfect Sandwich, A Real NYC Classic",paragraph:"Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."},
+    {src:"https://www.w3schools.com/w3images/steak.jpg",heading:"Let Me Tell You About This Steak",paragraph:"Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum."},
+    {src:"https://www.w3schools.com/w3images/cherries.jpg",heading:"Cherries, interrupted",paragraph:"Lorem ipsum text praesent tincidunt ipsum lipsum. What else?"},
+    {src:"https://www.w3schools.com/w3images/wine.jpg",heading:"Once Again, Robust Wine and Vegetable Pasta",paragraph:"Lorem ipsum text praesent tincidunt ipsum lipsum."},
+    {src:"https://www.w3schools.com/w3images/popsicle.jpg",heading:"All I Need Is a Popsicle",paragraph:"Lorem ipsum text praesent tincidunt ipsum lipsum."},
+    {src:"https://www.w3schools.com/w3images/salmon.jpg",heading:"Salmon For Your Skin",paragraph:"Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum."},
+    {src:"https://www.w3schools.com/w3images/sandwich.jpg",heading:"The Perfect Sandwich, A Real Classic",paragraph:"Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."},
+    {src:"https://www.w3schools.com/w3images/croissant.jpg",heading:"Le French",paragraph:"Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum."},
+]
+
+document.getElementById('menu').innerHTML = galleryData.map(image => 
+    `<div class="gallery-card">
+      <img class="gallery-card-img" src=${image.src} alt="">
+      <h2>${image.heading}</h2>
+      <p class="gallery-card-text" >Avatar: ${image.paragraph}</p>
+    </div>`
+).join('')
