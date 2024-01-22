cb342d9d07a3335f5f21aa599c30a194e25a4810
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-01-22 19:47:18 +0200

    add types

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-const App:React.FC = () => {
+const App = (): React.ReactNode => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
