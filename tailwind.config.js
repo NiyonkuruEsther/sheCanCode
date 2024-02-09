469999a2f2652a162307db067a4fda72a3ea4ae1
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-02-09 17:07:23 +0200

    add

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}", "./component/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-};
