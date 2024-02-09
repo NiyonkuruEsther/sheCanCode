89fd08c71ab7ab0e9bc738b47069fd03252925f8
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-02-10 01:08:03 +0200

    finalise

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ const rl = createInterface({
   output: stdout
 });
 
-let questionData = [
+let questionsData = [
   {
     question: "What is the capital of France?",
     options: ["Paris", "London", "Berlin", "Madrid"],
@@ -17,23 +17,45 @@ let questionData = [
     options: ["Mars", "Saturn", "Jupiter", "Neptune"],
     answer: "Jupiter"
   },
-
   {
     question: "What is the chemical symbol for gold?",
     options: ["Au", "Ag", "Cu", "Fe"],
     answer: "Au"
   }
 ];
-let i = 0;
 
-const generateQuestion = () => {
-  if (i < 3) {
-    rl.question(`Enter question ${i + 1}: `, (answer) => {
-      questionData.push({ question: answer });
-      console.log(questionData);
+let score = 0;
+let currentQuestion = 0;
+
+function displayQuestion() {
+  if (currentQuestion < questionsData.length) {
+    const current = questionsData[currentQuestion];
+    console.log(current.question);
+    current.options.map((option, index) => {
+      console.log(`${index + 1}. ${option}`);
     });
+    rl.question("Your option: ", (selectedAnswer) => {
+      checkAnswer(selectedAnswer.trim(), current.answer);
+    });
+  } else {
+    console.log(`You scored ${score}/${questionsData.length}`);
+    rl.close();
   }
-  generateQuestion();
-};
+}
 
-generateQuestion();
+function checkAnswer(selectedAnswer, correctAnswer) {
+  if (
+    selectedAnswer.toLowerCase() === correctAnswer.toLowerCase() ||
+    parseInt(selectedAnswer) ===
+      questionsData[currentQuestion].options.indexOf(correctAnswer) + 1
+  ) {
+    console.log("Correct!");
+    score++;
+  } else {
+    console.log("Incorrect!");
+    console.log("The correct answer is: ", correctAnswer);
+  }
+  currentQuestion++;
+  displayQuestion();
+}
+displayQuestion();
