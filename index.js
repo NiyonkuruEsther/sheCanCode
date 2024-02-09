a57cc65fe61c68d90bdcdda866d2dfb9f42045d5
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-02-09 15:49:02 +0200

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+const dogContainer = document.getElementById("dog-container");
+const imgElement = document.createElement("img");
+
+const loadImage = async () => {
+  const response = await fetch("https://random.dog/woof.json");
+  const data = await response.json();
+  const imgUrl = data.url;
+
+  if (imgUrl.includes(".mp4")) {
+    loadImage();
+  } else {
+    imgElement.src = imgUrl;
+    imgElement.style.width = "300px";
+    imgElement.style.height = "300px";
+    dogContainer.appendChild(imgElement);
+  }
+};
+
+document.getElementById("btn").addEventListener("click", loadImage);
