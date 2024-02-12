e464c59f969e8a93d6b8ca932c7574af6d359366
NiyonkuruEsther <estherniyonkuru123@gmail.com>
2024-02-12 17:02:59 +0200

    final push

--- my-app/App.js
+++ my-app/App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <Text>What is it like to live in this world!</Text>
-      <StatusBar style={{ backgroundColor: "black" }} />
-    </View>
+    <ScrollView>
+      <SafeAreaView>
+        <View style={[styles.nav, styles.container]}>
+          <Text style={styles.navText}>Todo</Text>
+        </View>
+        <ScrollView style={styles.todoListItems}>
+          <Text style={styles.todoListTitle}>Todo list items</Text>
+          <View style={styles.todoListItemContainer}>
+            <Text style={styles.todoListItem}>
+              What is it like to live in this world!
+            </Text>
+          </View>
+        </ScrollView>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "flex-end",
+    justifyContent: "center",
+    paddingRight: 30
+  },
+  nav: {
+    backgroundColor: "#FF5733",
+    height: 80
+  },
+  navText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 30
+  },
+  todoListItems: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    // flexDirection: "column",
+    padding: 20
+  },
+  todoListTitle: {
+    fontSize: 30,
+    paddingBottom: 20
+  },
+  todoListItemContainer: {
+    borderRadius: 8,
+    borderWidth: 2,
+    borderStyle: "dashed",
+    borderColor: "black",
+    marginBottom: 20
+  },
+  todoListItem: { fontSize: 20, paddingHorizontal: 20, paddingVertical: 10 }
 });
